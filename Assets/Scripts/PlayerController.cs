cd137e0a61915c8cfdae2a72b7414cdc3ec98a4f
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-28 22:53:08 -0500

    Basic Scene | L/R Movement on PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+	}
+}
