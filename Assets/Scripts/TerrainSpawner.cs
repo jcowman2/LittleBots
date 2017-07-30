cc6315ec6065aba43ef4e3b1eda9b871e9f17f9e
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-29 23:59:01 -0500

    Camera Bounds Checking

---
+++ Assets/Scripts/TerrainSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour {
+
+    [ReadOnly]
+    public Vector3 topLeft;
+
+    [ReadOnly]
+    public Vector3 bottomLeft;
+
+    [ReadOnly]
+    public Vector3 topRight;
+
+    [ReadOnly]
+    public Vector3 bottomRight;
+
+    private new Camera camera;
+
+    void Start () {
+        camera = GetComponent<Camera>();
+        UpdateCorners();
+	}
+	
+	
+	void Update () {
+        UpdateCorners();
+	}
+
+    void UpdateCorners() {
+        topLeft = camera.ViewportToWorldPoint(new Vector3(0, 1));
+        bottomLeft = camera.ViewportToWorldPoint(new Vector3(0, 0));
+        topRight = camera.ViewportToWorldPoint(new Vector3(1, 1));
+        bottomRight = camera.ViewportToWorldPoint(new Vector3(1, 0));
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(topLeft, 0.5f);
+        Gizmos.DrawSphere(bottomLeft, 0.5f);
+        Gizmos.DrawSphere(topRight, 0.5f);
+        Gizmos.DrawSphere(bottomRight, 0.5f);
+    }
+}
