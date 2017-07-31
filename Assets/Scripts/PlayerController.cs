8651f0d7e3fe3503f085e6a9da9907ba59710cc2
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-30 23:28:19 -0500

    Smooth Rotating Ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //** Base Movement **//
     public float maxSpeed = 10f;
     public float jumpForce = 700f;
-
     [ReadOnly]
     public bool grounded = false;
     [ReadOnly]
     public bool jumped = false;
 
+    //** Collision Detection **//
     public Transform groundCheck;
     public float groundRadius = 0.2f;
     public LayerMask groundMask;
+
+    //** Ball Movement **//
+    public Transform ballSprite;
+    public float ballAccelerateTime;
+    public float ballTopRotateSpeed;
+    [ReadOnly]
+    public float currentBallSpeed;
+    [ReadOnly]
+    public bool ballMoving;
 
     private Rigidbody2D rb;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        ballMoving = false;
     }
 
     private void Update () {
@@ -39,5 +50,11 @@ public class PlayerController : MonoBehaviour {
 
         float move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
-	}
+
+        ballMoving = Mathf.Abs(move) > 0;
+        currentBallSpeed = move * ballTopRotateSpeed;
+
+        ballSprite.Rotate(ballSprite.forward, currentBallSpeed * -1);
+
+    }
 }
