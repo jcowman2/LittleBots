233789a431935b74c528f77074b0abe3681715c6
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-30 23:49:18 -0500

    Rusty's Alive!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,8 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
 
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
+        rb = GameObject.FindGameObjectWithTag(R.PLAYER).GetComponent<Rigidbody2D>();
         ballMoving = false;
     }
 
