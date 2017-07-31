be54423241a4c7936d94e6cfedc2900bef2a353e
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-31 14:40:36 -0500

    Background Lights Change

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -8,6 +8,15 @@ public class GameControl : MonoBehaviour {
     [ReadOnly]
     public GameObject player;
 
+    //** Charge Utilities **//
+    [ReadOnly]
+    public int chargeLevel; //between 0 and 100
+
+    public float dechargeTime = 0.5f; //Seconds for losing one point of charge
+
+    [ReadOnly]
+    public float timeSinceDecharge = 0f;
+
     //** Camera Endpoints **//
     [ReadOnly]
     public Vector3 topLeft;
@@ -25,16 +34,26 @@ public class GameControl : MonoBehaviour {
     public Vector3 cameraPos;
 
     private new Camera camera;
+    private BackgroundControl background;
 
     void Start () {
         player = GameObject.FindGameObjectWithTag(R.PLAYER);
         camera = GameObject.FindGameObjectWithTag(R.MAIN_CAMERA).GetComponent<Camera>();
+        background = GetComponent<BackgroundControl>();
+
         UpdateCorners();
+        chargeLevel = 100;
     }
 
     void Update() {
         UpdateCorners();
         cameraPos = camera.transform.position;
+
+        timeSinceDecharge += Time.deltaTime;
+        if (timeSinceDecharge >= dechargeTime) {
+            changeChargeLevel(-1);
+            timeSinceDecharge = 0;
+        }
     }
 
     void UpdateCorners () {
@@ -75,5 +94,14 @@ public class GameControl : MonoBehaviour {
         Gizmos.DrawSphere(bottomLeft, 0.5f);
         Gizmos.DrawSphere(topRight, 0.5f);
         Gizmos.DrawSphere(bottomRight, 0.5f);
+    }
+
+    public int changeChargeLevel(int amount) {
+        int newChargeLevel = Mathf.Min(chargeLevel + amount, 100);
+
+        background.SetSpriteIndex(Mathf.Min(newChargeLevel / 10 + 1, 10));
+
+        chargeLevel = newChargeLevel;
+        return newChargeLevel;
     }
 }
